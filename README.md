99809cb1479e8c4b52c19865947edd8c8a32b6db
Deven Blake <blakedevendesu@gmail.com>
2020-09-06 23:43:04 -0400

    Finished ascii.c and the README.

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # cstuff
+
 Short C programs.
+
+## requirements
+
+- an ANSI C compiler compatible with the C89 specification.
+- a UNIX-like environment (AIX, BSDs, Linuxes, macOS, and Solaris should all work unless otherwise indicated in a program's comments).
+
+## compiling
+
+Make sure you have an ANSI C compiler (or a more specific one if specified in the comments of the file you're compiling).
+
+`cc [programname].c`
+
+## installing
+
+#### for all users
+
+`mv a.out /usr/bin/[programname]`
+
+#### for the local user
+
+```
+mv a.out $HOME/bin/[programname]
+[open your preferred shell]
+export PATH=$PATH":$HOME/bin"
+```
+
+## uninstalling
+
+#### for all users
+
+`rm /usr/bin/[programname]`
+
+#### for the local user
+
+```
+rm $HOME/bin/[programname]
+exit
+```
--- ascii.c
+++ ascii.c
@@ -14,25 +14,30 @@
 		./a.out
 		prints the text values between and including 32 (' ') and 127
 		('~'), the printable values in the ASCII range.
+
+	Sample output:
+		> ./a.out 49 51
+		DEC	HEX	OCT	CHAR
+		49	31	61	0
+		50	32	62	1
+		51	33	63	2
 */
 
 #include <stdio.h>
 #include <stdlib.h>
-/* change this to change the printf format */
+/*           V-- change this to change the printf format */
 #define HEAD "DEC\tHEX\tOCT\tCHAR\n"
 #define FORM "%d\t%X\t%o\t%c\n"
 
 int main(int argc, char *argv[]) {
 	int min, max;
 	if(argc > 1)	min = atoi(argv[1]);
-	else		min = 32;
+	else		min = 32; /* ' ' */
 	printf(HEAD);
-	if(argc == 2) {
-		printf(FORM, min, min, min, min);
-	}
+	if(argc == 2)	printf(FORM, min, min, min, min);
 	else {
 		if(argc == 3)	max = atoi(argv[2]);
-		else		max = 127;
+		else		max = 127; /* '~' */
 		for(int i = min - 1; i < max;)	printf(FORM, i, i, i, i++);
 	}
 	return 0;
